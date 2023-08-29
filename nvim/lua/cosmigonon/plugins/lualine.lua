cc385244684a046890feacb9e547765c5fd52882
Cosmigonón <mafretd@yahoo.com>
2023-08-28 23:01:17 -0500

    fzf pluggin

---
+++ nvim/lua/cosmigonon/plugins/lualine.lua
@@ -0,0 +1,23 @@
+return { "nvim-lualine/lualine.nvim",    
+    config = function()
+        local lualine = require("lualine")
+        lualine.setup({
+            options = {
+                theme = "auto",
+                icons_enabled = true,
+            },
+            sections = {
+                lualine_x = {
+                    {
+                        require("lazy.status").updates,
+                        cond = require("lazy.status").has_updates,
+                    },
+                    {"encoding"}, 
+                    {"fileformat"}, 
+                    {"filetype"},
+                }
+            }
+       })
+    end,
+        
+}
---
+++ nvim/lua/cosmigonon/plugins/nvim-treesitter.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local treesitter = require("nvim-treesitter.configs")
+            treesitter.setup({
+                highlight = {
+                    enable = true,
+                },
+                indent = {enable = true},
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "python",
+                    "java",
+                    "html",
+                    "css",
+                    "gitignore",
+                    "bash",
+                    "javascript",
+                    "json",
+                    "markdown",
+                    "xml",
+                },
+                auto_install = true,
+            })
+        end,
+    },
+}
