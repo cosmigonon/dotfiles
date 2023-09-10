e13a59b7b99155642accd3c064f8543f802f3009
Cosmigonón <mafretd@yahoo.com>
2023-09-10 16:37:45 -0500

    changed font style types default options

--- nvim/lua/cosmigonon/plugins/colorscheme.lua
+++ nvim/lua/cosmigonon/plugins/colorscheme.lua
@@ -1,23 +1,26 @@
+-- Plugin: Colorschemes
+-- https://github.com/loctvl842/monokai-pro.nvim
+
 return {
-       "loctvl842/monokai-pro.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("monokai-pro").setup({
-            transparent_background = false,
-            terminal_colors = true,
-            styles = {
-                comment = {italic = true},
-                keyword = {italic = false},
-                type = {italic = true},
-                storageclass = {italic = false},
-                parameter = {italic = false},
-                annotation = {italic = true},
-                tag_attribute = {italic = true},
-            },
-            filter = "pro",
-            inc_search = "background",
-        })
-        vim.cmd([[colorscheme monokai-pro]])
-        end,
+	"loctvl842/monokai-pro.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("monokai-pro").setup({
+			transparent_background = false,
+			terminal_colors = true,
+			styles = {
+				comment = { italic = true },
+				keyword = { italic = false },
+				type = { italic = false },
+				storageclass = { italic = false },
+				parameter = { italic = false },
+				annotation = { italic = false },
+				tag_attribute = { italic = false },
+			},
+			filter = "pro",
+			inc_search = "background",
+		})
+		vim.cmd([[colorscheme monokai-pro]])
+	end,
 }
